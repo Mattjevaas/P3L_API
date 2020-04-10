e3fb0a7ff93003235439a83ac53e95ef5250fe36
agungskak22 <agungskak22@gmail.com>
2020-04-11 04:23:26 +0700

    fix SupplierController

--- app/Http/Controllers/SupplierController.php
+++ app/Http/Controllers/SupplierController.php
@@ -3,6 +3,8 @@ namespace App\Http\Controllers;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+
 use App\Supplier;
 
 class SupplierController extends Controller
